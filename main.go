589b076ec2a2729a8c7809ba1efb4bbf0d702198
y-du <42994541+y-du@users.noreply.github.com>
2022-09-08 12:51:52 +0200

    add server and handle shutdown

--- main.go
+++ main.go
@@ -17,14 +17,19 @@
 package main
 
 import (
+	"context"
 	"deployment-manager/manager/api"
 	"deployment-manager/manager/handler"
-	"deployment-manager/manager/handler/gin-engine"
+	"deployment-manager/manager/handler/api-engine"
 	"deployment-manager/util"
 	"deployment-manager/util/configuration"
 	"deployment-manager/util/logger"
 	"fmt"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
@@ -35,11 +40,30 @@ func main() {
 		os.Exit(1)
 	}
 	logger.InitLogger(config.Logger.Level, "[DM] ", false, config.Logger.Utc)
+	engine := api_engine.NewEngine(config.StaticOrigins, config.Logger.Level)
+	api.SetRoutes(engine)
+	server := http.Server{
+		Handler: engine,
+	}
 	listener, err := handler.NewUnixListener(config.SocketPath)
 	if err != nil {
 		logger.Fatal(err)
 	}
-	engine := gin_engine.NewEngine(config.StaticOrigins, config.Logger.Level)
-	api.SetRoutes(engine)
-
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		sig := <-shutdown
+		logger.WarningF("received signal '%s'", sig)
+		logger.Info("initiating shutdown ...")
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		if err := server.Shutdown(ctx); err != nil {
+			logger.Error("server forced to shutdown: ", err)
+		}
+	}()
+	logger.Info("starting server ...")
+	if err := server.Serve(listener); err != nil && err != http.ErrServerClosed {
+		logger.Fatal("starting server failed: ", err)
+	}
+	logger.Info("shutdown complete")
 }
