8c5209f63405ccbc286b071c0abdbd5ec672bf32
y-du <42994541+y-du@users.noreply.github.com>
2022-09-13 14:49:22 +0200

    add list containers function

--- manager/ce-handler/docker/docker.go
+++ manager/ce-handler/docker/docker.go
@@ -19,6 +19,9 @@ package docker
 import (
 	"context"
 	"deployment-manager/manager/ce-handler/itf"
+	"deployment-manager/util"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/filters"
 	"github.com/docker/docker/client"
 )
 
@@ -32,6 +35,46 @@ func New() (*Docker, error) {
 		return nil, err
 	}
 	return &Docker{client: c}, nil
+}
+
+func (d *Docker) ListContainers(ctx context.Context, filter map[string]string) (map[string]*itf.Container, error) {
+	var f filters.Args
+	if filter != nil && len(filter) > 0 {
+		f = filters.NewArgs()
+		for k, v := range filter {
+			f.Add(k, v)
+		}
+	}
+	if cl, err := d.client.ContainerList(ctx, types.ContainerListOptions{Filters: f}); err != nil {
+		return nil, err
+	} else {
+		cm := make(map[string]*itf.Container, len(cl))
+		for _, c := range cl {
+			container := &itf.Container{
+				ID:       c.ID,
+				ImageID:  c.ImageID,
+				Image:    c.Image,
+				Status:   c.State,
+				Mounts:   parseContainerMounts(c.Mounts),
+				Labels:   c.Labels,
+				Networks: parseContainerNetworks(c.NetworkSettings.Networks),
+			}
+			if ci, err := d.client.ContainerInspect(ctx, c.ID); err != nil {
+				util.Logger.Error(err)
+			} else {
+				container.Name = ci.Name
+				container.StartPolicy = ci.HostConfig.RestartPolicy.Name
+				container.CreatedAt = ci.Created
+				container.StartedAt = ci.State.StartedAt
+				container.Hostname = ci.Config.Hostname
+				container.Env = parseContainerEnvVars(ci.Config.Env)
+				container.Networks = parseContainerNetworks(ci.NetworkSettings.Networks)
+				container.Ports = parseContainerPorts(ci.Config.ExposedPorts, ci.NetworkSettings.Ports)
+			}
+			cm[c.ID] = container
+		}
+		return cm, nil
+	}
 }
 
 func (d *Docker) ContainerInfo(ctx context.Context, id string) (*itf.Container, error) {
