c529be64d497a132460fcfe4dc54cbab5df943c3
y-du <42994541+y-du@users.noreply.github.com>
2022-09-16 13:29:14 +0200

    add functions

--- manager/handler/docker/util.go
+++ manager/handler/docker/util.go
@@ -19,7 +19,10 @@ package docker
 import (
 	"deployment-manager/manager/itf"
 	"deployment-manager/util"
+	"errors"
+	"fmt"
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/mount"
 	"github.com/docker/docker/api/types/network"
 	"github.com/docker/go-connections/nat"
 	"strconv"
@@ -64,7 +67,7 @@ func parseContainerPorts(portSet nat.PortSet, portMap nat.PortMap) (ports []itf.
 			ports = append(ports, p)
 			set[p.KeyStr()] = struct{}{}
 		}
-		for port, _ := range portSet {
+		for port := range portSet {
 			if _, ok := set[string(port)]; !ok {
 				ports = append(ports, itf.Port{
 					Number:   port.Int(),
@@ -103,10 +106,91 @@ func parseContainerEnvVars(ev []string) (env map[string]string) {
 	return
 }
 
+func genContainerEnvVars(ev map[string]string) (env []string) {
+	if len(ev) > 0 {
+		for key, val := range ev {
+			env = append(env, fmt.Sprintf("%s=%s", key, val))
+		}
+	}
+	return
+}
+
 func parseStopTimeout(t *int) *time.Duration {
 	if t != nil {
 		d := time.Duration(*t * int(time.Second))
 		return &d
+	}
+	return nil
+}
+
+func getStopTimeout(d *time.Duration) *int {
+	if d != nil {
+		t := int(d.Seconds())
+		return &t
+	}
+	return nil
+}
+
+func getPorts(ports []itf.Port) (nat.PortMap, error) {
+	pm := make(nat.PortMap)
+	set := make(map[string]struct{})
+	for _, p := range ports {
+		if _, ok := set[p.KeyStr()]; ok {
+			return pm, errors.New("port duplicate")
+		}
+		set[p.KeyStr()] = struct{}{}
+		port, err := nat.NewPort(portTypeRMap[p.Protocol], strconv.FormatInt(int64(p.Number), 10))
+		if err != nil {
+			return pm, err
+		}
+		var bindings []nat.PortBinding
+		for _, binding := range p.Bindings {
+			bindings = append(bindings, nat.PortBinding{
+				HostIP:   binding.Interface,
+				HostPort: strconv.FormatInt(int64(binding.Number), 10),
+			})
+		}
+		pm[port] = bindings
+	}
+	return pm, nil
+}
+
+func getMounts(mounts []itf.Mount) ([]mount.Mount, error) {
+	var msl []mount.Mount
+	set := make(map[string]struct{})
+	for i := 0; i < len(mounts); i++ {
+		m := mounts[i]
+		if _, ok := set[m.KeyStr()]; ok {
+			return msl, errors.New("mount duplicate")
+		}
+		set[m.KeyStr()] = struct{}{}
+		mnt := mount.Mount{
+			Type:     mountTypeRMap[m.Type],
+			Source:   m.Source,
+			Target:   m.Target,
+			ReadOnly: m.ReadOnly,
+		}
+		switch m.Type {
+		case itf.VolumeMount:
+			mnt.VolumeOptions = &mount.VolumeOptions{Labels: m.Labels}
+		case itf.TmpfsMount:
+			mnt.TmpfsOptions = &mount.TmpfsOptions{
+				SizeBytes: m.Size,
+				Mode:      m.Mode,
+			}
+		}
+		msl = append(msl, mnt)
+	}
+	return msl, nil
+}
+
+func checkNetworks(n []itf.ContainerNet) error {
+	set := make(map[string]struct{})
+	for _, net := range n {
+		if _, ok := set[net.Name]; ok {
+			return errors.New("network duplicate")
+		}
+		set[net.Name] = struct{}{}
 	}
 	return nil
 }
