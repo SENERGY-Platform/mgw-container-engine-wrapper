59fc86664e7589d24c01af166079e64d98db4aa2
y-du <42994541+y-du@users.noreply.github.com>
2022-09-22 15:48:59 +0200

    refactor

--- manager/handler/docker/container.go
+++ manager/handler/docker/container.go
@@ -29,95 +29,93 @@ import (
 )
 
 func (d *Docker) ListContainers(ctx context.Context, filter [][2]string) ([]itf.Container, error) {
-	if cl, err := d.client.ContainerList(ctx, types.ContainerListOptions{All: true, Filters: util.GenFilterArgs(filter)}); err != nil {
-		return nil, err
-	} else {
-		var csl []itf.Container
-		for _, c := range cl {
-			ctr := itf.Container{
-				ID:       c.ID,
-				State:    util.StateMap[c.State],
-				ImageID:  c.ImageID,
-				Labels:   c.Labels,
-				Mounts:   util.ParseMountPoints(c.Mounts),
-				Networks: util.ParseEndpointSettings(c.NetworkSettings.Networks),
-			}
-			if ci, err := d.client.ContainerInspect(ctx, c.ID); err != nil {
+	var csl []itf.Container
+	cl, err := d.client.ContainerList(ctx, types.ContainerListOptions{All: true, Filters: util.GenFilterArgs(filter)})
+	if err != nil {
+		return csl, err
+	}
+	for _, c := range cl {
+		ctr := itf.Container{
+			ID:       c.ID,
+			State:    util.StateMap[c.State],
+			ImageID:  c.ImageID,
+			Labels:   c.Labels,
+			Mounts:   util.ParseMountPoints(c.Mounts),
+			Networks: util.ParseEndpointSettings(c.NetworkSettings.Networks),
+		}
+		if ci, err := d.client.ContainerInspect(ctx, c.ID); err != nil {
+			dmUtil.Logger.Error(err)
+		} else {
+			ctr.Name = ci.Name
+			if tc, err := util.ParseTimestamp(ci.Created); err != nil {
 				dmUtil.Logger.Error(err)
 			} else {
-				ctr.Name = ci.Name
-				if tc, err := util.ParseTimestamp(ci.Created); err != nil {
+				ctr.Created = tc
+			}
+			if c.State == "running" {
+				if ts, err := util.ParseTimestamp(ci.State.StartedAt); err != nil {
 					dmUtil.Logger.Error(err)
 				} else {
-					ctr.Created = tc
-				}
-				if c.State == "running" {
-					if ts, err := util.ParseTimestamp(ci.State.StartedAt); err != nil {
-						dmUtil.Logger.Error(err)
-					} else {
-						ctr.Started = &ts
-					}
-				}
-				ctr.Image = ci.Config.Image
-				ctr.EnvVars = util.ParseEnv(ci.Config.Env)
-				ctr.Ports = util.ParsePortSetAndMap(ci.Config.ExposedPorts, ci.NetworkSettings.Ports)
-				if len(ci.HostConfig.Mounts) > 0 {
-					ctr.Mounts = util.ParseMounts(ci.HostConfig.Mounts)
-				}
-				ctr.Networks = util.ParseEndpointSettings(ci.NetworkSettings.Networks)
-				ctr.RunConfig = itf.RunConfig{
-					RestartStrategy: util.RestartPolicyMap[ci.HostConfig.RestartPolicy.Name],
-					Retries:         ci.HostConfig.RestartPolicy.MaximumRetryCount,
-					RemoveAfterRun:  ci.HostConfig.AutoRemove,
-					StopTimeout:     util.ParseStopTimeout(ci.Config.StopTimeout),
+					ctr.Started = &ts
 				}
 			}
-			csl = append(csl, ctr)
+			ctr.Image = ci.Config.Image
+			ctr.EnvVars = util.ParseEnv(ci.Config.Env)
+			ctr.Ports = util.ParsePortSetAndMap(ci.Config.ExposedPorts, ci.NetworkSettings.Ports)
+			if len(ci.HostConfig.Mounts) > 0 {
+				ctr.Mounts = util.ParseMounts(ci.HostConfig.Mounts)
+			}
+			ctr.Networks = util.ParseEndpointSettings(ci.NetworkSettings.Networks)
+			ctr.RunConfig = itf.RunConfig{
+				RestartStrategy: util.RestartPolicyMap[ci.HostConfig.RestartPolicy.Name],
+				Retries:         ci.HostConfig.RestartPolicy.MaximumRetryCount,
+				RemoveAfterRun:  ci.HostConfig.AutoRemove,
+				StopTimeout:     util.ParseStopTimeout(ci.Config.StopTimeout),
+			}
 		}
-		return csl, nil
+		csl = append(csl, ctr)
 	}
+	return csl, nil
 }
 
 func (d *Docker) ContainerInfo(ctx context.Context, id string) (itf.Container, error) {
-	var ctr itf.Container
-	if c, err := d.client.ContainerInspect(ctx, id); err != nil {
+	ctr := itf.Container{}
+	c, err := d.client.ContainerInspect(ctx, id)
+	if err != nil {
 		return ctr, err
+	}
+	var mts []itf.Mount
+	if len(c.HostConfig.Mounts) > 0 {
+		mts = util.ParseMounts(c.HostConfig.Mounts)
 	} else {
-		var mts []itf.Mount
-		if len(c.HostConfig.Mounts) > 0 {
-			mts = util.ParseMounts(c.HostConfig.Mounts)
-		} else {
-			mts = util.ParseMountPoints(c.Mounts)
-		}
-		ctr = itf.Container{
-			ID:       c.ID,
-			Name:     c.Name,
-			State:    util.StateMap[c.State.Status],
-			Image:    c.Config.Image,
-			ImageID:  c.Image,
-			EnvVars:  util.ParseEnv(c.Config.Env),
-			Labels:   c.Config.Labels,
-			Mounts:   mts,
-			Ports:    util.ParsePortSetAndMap(c.Config.ExposedPorts, c.NetworkSettings.Ports),
-			Networks: util.ParseEndpointSettings(c.NetworkSettings.Networks),
-			RunConfig: itf.RunConfig{
-				RestartStrategy: util.RestartPolicyMap[c.HostConfig.RestartPolicy.Name],
-				Retries:         c.HostConfig.RestartPolicy.MaximumRetryCount,
-				RemoveAfterRun:  c.HostConfig.AutoRemove,
-				StopTimeout:     util.ParseStopTimeout(c.Config.StopTimeout),
-			},
-		}
-		if tc, err := util.ParseTimestamp(c.Created); err != nil {
+		mts = util.ParseMountPoints(c.Mounts)
+	}
+	ctr.ID = c.ID
+	ctr.Name = c.Name
+	ctr.State = util.StateMap[c.State.Status]
+	ctr.Image = c.Config.Image
+	ctr.ImageID = c.Image
+	ctr.EnvVars = util.ParseEnv(c.Config.Env)
+	ctr.Labels = c.Config.Labels
+	ctr.Mounts = mts
+	ctr.Ports = util.ParsePortSetAndMap(c.Config.ExposedPorts, c.NetworkSettings.Ports)
+	ctr.Networks = util.ParseEndpointSettings(c.NetworkSettings.Networks)
+	ctr.RunConfig = itf.RunConfig{
+		RestartStrategy: util.RestartPolicyMap[c.HostConfig.RestartPolicy.Name],
+		Retries:         c.HostConfig.RestartPolicy.MaximumRetryCount,
+		RemoveAfterRun:  c.HostConfig.AutoRemove,
+		StopTimeout:     util.ParseStopTimeout(c.Config.StopTimeout),
+	}
+	if tc, err := util.ParseTimestamp(c.Created); err != nil {
+		dmUtil.Logger.Error(err)
+	} else {
+		ctr.Created = tc
+	}
+	if c.State.Status == "running" {
+		if ts, err := util.ParseTimestamp(c.State.StartedAt); err != nil {
 			dmUtil.Logger.Error(err)
 		} else {
-			ctr.Created = tc
-		}
-		if c.State.Status == "running" {
-			if ts, err := util.ParseTimestamp(c.State.StartedAt); err != nil {
-				dmUtil.Logger.Error(err)
-			} else {
-				ctr.Started = &ts
-			}
+			ctr.Started = &ts
 		}
 	}
 	return ctr, nil
@@ -204,7 +202,6 @@ func (d *Docker) ContainerRestart(ctx context.Context, id string) error {
 }
 
 func (d *Docker) ContainerLog(ctx context.Context, id string, logOpt itf.LogOptions) (io.ReadCloser, error) {
-	var lr util.LogReader
 	clo := types.ContainerLogsOptions{
 		ShowStdout: true,
 		ShowStderr: true,
@@ -220,7 +217,7 @@ func (d *Docker) ContainerLog(ctx context.Context, id string, logOpt itf.LogOpti
 	}
 	rc, err := d.client.ContainerLogs(ctx, id, clo)
 	if err != nil {
-		return &lr, err
+		return nil, err
 	}
 	return util.NewLogReader(rc), nil
 }
