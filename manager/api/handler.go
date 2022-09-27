d370836276b4e32ff81cd789833134cdbbcf6d3f
y-du <42994541+y-du@users.noreply.github.com>
2022-09-27 09:47:49 +0200

    implement get container log

--- manager/api/handler.go
+++ manager/api/handler.go
@@ -20,7 +20,9 @@ import (
 	"deployment-manager/manager/itf"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"io"
 	"net/http"
+	"time"
 )
 
 func (a Api) GetContainers(gc *gin.Context) {
@@ -72,7 +74,69 @@ func (a Api) GetContainer(gc *gin.Context) {
 }
 
 func (a Api) GetContainerLog(gc *gin.Context) {
-
+	id := gc.Param(containerParam)
+	query := ContainerLogQuery{}
+	if err := gc.ShouldBindQuery(&query); err != nil {
+		gc.Status(http.StatusBadRequest)
+		_ = gc.Error(err)
+		return
+	}
+	logOptions := itf.LogOptions{MaxLines: query.MaxLines}
+	if query.Since != "" {
+		since, err := time.Parse(time.RFC3339Nano, query.Since)
+		if err != nil {
+			gc.Status(http.StatusBadRequest)
+			_ = gc.Error(err)
+			return
+		}
+		logOptions.Since = &since
+	}
+	if query.Until != "" {
+		until, err := time.Parse(time.RFC3339Nano, query.Until)
+		if err != nil {
+			gc.Status(http.StatusBadRequest)
+			_ = gc.Error(err)
+			return
+		}
+		logOptions.Until = &until
+	}
+	rc, err := a.ceHandler.ContainerLog(gc.Request.Context(), id, logOptions)
+	if err != nil {
+		_ = gc.Error(err)
+		return
+	}
+	defer rc.Close()
+	gc.Status(http.StatusOK)
+	gc.Header("Transfer-Encoding", "chunked")
+	gc.Header("Content-Type", gin.MIMEPlain)
+	for {
+		var b = make([]byte, 204800)
+		n, rErr := rc.Read(b)
+		if rErr != nil {
+			if rErr == io.EOF {
+				if n > 0 {
+					_, wErr := gc.Writer.Write(b[:n])
+					if wErr != nil {
+						gc.Status(http.StatusInternalServerError)
+						_ = gc.Error(wErr)
+						return
+					}
+					gc.Writer.Flush()
+				}
+				break
+			}
+			gc.Status(http.StatusInternalServerError)
+			_ = gc.Error(rErr)
+			return
+		}
+		_, wErr := gc.Writer.Write(b)
+		if wErr != nil {
+			gc.Status(http.StatusInternalServerError)
+			_ = gc.Error(wErr)
+			return
+		}
+		gc.Writer.Flush()
+	}
 }
 
 func (a Api) GetImages(gc *gin.Context) {
