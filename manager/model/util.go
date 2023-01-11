7fd1c661bb52f40110baffba26a6cc9ee3795c46
y-du <42994541+y-du@users.noreply.github.com>
2023-01-11 14:10:37 +0100

    refactor

--- manager/model/util.go
+++ manager/model/util.go
@@ -63,21 +63,17 @@ func (n *NetworkType) UnmarshalJSON(b []byte) (err error) {
 	return
 }
 
-func (r *RestartStrategy) parse(s string) error {
+func (r *RestartStrategy) UnmarshalJSON(b []byte) (err error) {
+	var s string
+	if err = json.Unmarshal(b, &s); err != nil {
+		return
+	}
 	if st, ok := RestartStrategyMap[s]; ok {
 		*r = st
 	} else {
 		return fmt.Errorf("unknown restart strategy '%s'", s)
 	}
 	return nil
-}
-
-func (r *RestartStrategy) UnmarshalJSON(b []byte) (err error) {
-	var s string
-	if err = json.Unmarshal(b, &s); err != nil {
-		return
-	}
-	return r.parse(s)
 }
 
 func (m *MountType) UnmarshalJSON(b []byte) (err error) {
@@ -106,21 +102,17 @@ func (i *IPAddr) UnmarshalJSON(b []byte) (err error) {
 	return
 }
 
-func (d *Duration) parse(s string) error {
+func (d *Duration) UnmarshalJSON(b []byte) error {
+	var s string
+	if err := json.Unmarshal(b, &s); err != nil {
+		return err
+	}
 	if dur, err := time.ParseDuration(s); err != nil {
 		return err
 	} else {
 		d.Duration = dur
 	}
 	return nil
-}
-
-func (d *Duration) UnmarshalJSON(b []byte) error {
-	var s string
-	if err := json.Unmarshal(b, &s); err != nil {
-		return err
-	}
-	return d.parse(s)
 }
 
 func (d Duration) MarshalJSON() ([]byte, error) {
@@ -140,21 +132,17 @@ func (c *ContainerSetState) UnmarshalJSON(b []byte) (err error) {
 	return
 }
 
-func (m *FileMode) parse(s string) error {
+func (m *FileMode) UnmarshalJSON(b []byte) (err error) {
+	var s string
+	if err = json.Unmarshal(b, &s); err != nil {
+		return
+	}
 	i, err := strconv.ParseUint(s, 8, 32)
 	if err != nil {
 		return err
 	}
 	m.FileMode = fs.FileMode(i)
 	return nil
-}
-
-func (m *FileMode) UnmarshalJSON(b []byte) (err error) {
-	var s string
-	if err = json.Unmarshal(b, &s); err != nil {
-		return
-	}
-	return m.parse(s)
 }
 
 func (m FileMode) MarshalJSON() ([]byte, error) {
