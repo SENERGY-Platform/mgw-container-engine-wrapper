0b97dc55ef56502e4478a526375950827c6c4582
y-du <42994541+y-du@users.noreply.github.com>
2023-01-27 11:29:00 +0100

    init

---
+++ wrapper/handler/job/job.go
@@ -0,0 +1,76 @@
+package job
+
+import (
+	"context"
+	"github.com/SENERGY-Platform/mgw-container-engine-wrapper/wrapper/model"
+	"github.com/google/uuid"
+	"sync"
+	"time"
+)
+
+type Job struct {
+	mu    sync.RWMutex
+	meta  model.Job
+	tFunc func()
+	ctx   context.Context
+	cFunc context.CancelFunc
+}
+
+func NewJob(ctx context.Context, id uuid.UUID) *Job {
+	c, cf := context.WithCancel(ctx)
+	return &Job{
+		meta: model.Job{
+			ID:      id,
+			Created: model.Time(time.Now().UTC()),
+		},
+		ctx:   c,
+		cFunc: cf,
+	}
+}
+
+func (j *Job) SetTarget(f func()) {
+	j.tFunc = f
+}
+
+func (j *Job) CallTarget(cbk func()) {
+	j.tFunc()
+	cbk()
+}
+
+func (j *Job) IsCanceled() bool {
+	j.mu.RLock()
+	defer j.mu.RUnlock()
+	return j.ctx.Err() == context.Canceled
+}
+
+func (j *Job) Cancel() {
+	j.cFunc()
+	j.mu.Lock()
+	j.meta.Canceled = model.Time(time.Now().UTC())
+	j.mu.Unlock()
+}
+
+func (j *Job) Meta() model.Job {
+	j.mu.RLock()
+	defer j.mu.RUnlock()
+	return j.meta
+}
+
+func (j *Job) SetStarted() {
+	j.mu.Lock()
+	j.meta.Started = model.Time(time.Now().UTC())
+	j.mu.Unlock()
+}
+
+func (j *Job) SetCompleted() {
+	j.mu.Lock()
+	j.meta.Completed = model.Time(time.Now().UTC())
+	j.mu.Unlock()
+}
+
+func (j *Job) SetResult(ref string, err error) {
+	j.mu.Lock()
+	j.meta.Ref = ref
+	j.meta.Error = err.Error()
+	j.mu.Unlock()
+}
