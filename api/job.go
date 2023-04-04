b5701a7a94998f609eec9f39a74a9f7573ebd9f1
y-du <42994541+y-du@users.noreply.github.com>
2023-04-04 11:42:33 +0200

    use new job handler

--- api/job.go
+++ api/job.go
@@ -14,11 +14,6 @@ func (a *Api) GetJob(_ context.Context, id string) (*itf.Job, error) {
 	return a.jobHandler.Get(id)
 }
 
-func (a *Api) CancelJob(ctx context.Context, id string) error {
-	j, err := a.jobHandler.Get(id)
-	if err != nil {
-		return err
-	}
-	j.Cancel()
-	return nil
+func (a *Api) CancelJob(_ context.Context, id string) error {
+	return a.jobHandler.Cancel(id)
 }
