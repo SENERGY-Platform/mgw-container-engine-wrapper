e6dd3cf13b90b82f5d6f589c3206b823021cce22
y-du <42994541+y-du@users.noreply.github.com>
2023-06-19 14:16:55 +0200

    rename socket

--- util/config.go
+++ util/config.go
@@ -59,7 +59,7 @@ func NewConfig(path *string) (*Config, error) {
 			Microseconds: true,
 		},
 		Socket: SocketConfig{
-			Path:     "/opt/mgw/sockets/cew.sock",
+			Path:     "/opt/mgw/sockets/ce_wrapper.sock",
 			GroupID:  os.Getgid(),
 			FileMode: 0660,
 		},
